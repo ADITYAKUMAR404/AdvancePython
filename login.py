8c31907b762c17c14b75b3a55b05f8309ca6944d
Aditya Kumar <140996835+ADITYAKUMAR404@users.noreply.github.com>
2026-03-19 11:16:54 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.lift()
+root.attributes('-topmost', True)
+root.after(1000, lambda: root.attributes('-topmost', False))
+root.title("Login Form")
+
+root.geometry('500x500+0+0')
+root.configure(background='#00704A')
+
+# image
+img = Image.open('amazon.jpg')
+resize_img = img.resize((100,70))
+img = ImageTk.PhotoImage(resize_img)
+
+img_label = Label(root, image=img)
+img_label.pack(pady=10, padx=20)
+
+# text label
+text_label = Label(root, text="Giet Bucks", font=('Arial', 18, 'bold'), bg='#00704A', fg='white')
+text_label.pack(pady=10, padx=20)
+
+email_label = Label(root, text="Email", font=('Arial', 18, 'bold'), bg='#00704A', fg='white')
+email_label.pack(pady=(20,5))
+
+email_entry = Entry(root, font=('Arial', 18, 'bold'), fg='white', bg='grey')
+email_entry.pack(pady=(5,10))
+
+password_label = Label(root, text="Password", font=('Arial', 18, 'bold'), bg='#00704A', fg='white')
+password_label.pack(pady=(20,5))
+
+password_entry = Entry(root, font=('Arial', 18, 'bold'), fg='white', bg='grey')
+password_entry.pack(pady=(5,10))
+
+login_btn = Button(root, text="Login", font=('Arial', 18, 'bold'), bg='#00704A', fg='white')
+login_btn.pack(pady=(5,10))
+
+root.mainloop()
